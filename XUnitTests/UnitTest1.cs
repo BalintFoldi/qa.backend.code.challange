d4b1b238b210b1fd810d8517d1373ab9c9903d14
BalintFoldi <balint.foldi@wundermanthompson.com>
2024-08-29 19:48:17 +0200

    WithdrawFundsAsync_ShouldDecreaseBalance_ByWithdrawalAmount unit test added

--- XUnitTests/UnitTest1.cs
+++ XUnitTests/UnitTest1.cs
@@ -74,5 +74,30 @@ namespace Betsson.OnlineWallets.Services
             Assert.Single(fakeRepository.Entries); // Ensure a new entry was added
             Assert.Equal(30m, fakeRepository.Entries[0].Amount); // The deposit amount
         }
+
+        [Fact]
+        public async Task WithdrawFundsAsync_ShouldDecreaseBalance_ByWithdrawalAmount()
+        {
+            //Arrange
+            var fakeRepository = new FakeOnlineWalletRepository
+            {
+                LastEntry = new OnlineWalletEntry
+                {
+                    BalanceBefore = 100m,
+                    Amount = 50m
+                }
+            };
+
+            var walletservice = new OnlineWalletService(fakeRepository);
+            var withdrawal = new Withdrawal { Amount = 20m };
+
+            //Act
+            Balance newBalance = await walletservice.WithdrawFundsAsync(withdrawal);
+
+            //Assert
+            Assert.Equal(130m, newBalance.Amount); // 150 - 20 = 130
+            Assert.Single(fakeRepository.Entries); //Ensure the new entry was added
+            Assert.Equal(-20m, fakeRepository.Entries[0].Amount); // The withdrawal amount should be negative
+        }
     }
 }
